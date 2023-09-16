4280c01947937774a4149ae5d2ac9fba6275fa2f
hashaam <hashammasood@gmail.com>
2023-09-17 00:32:05 +0500

    splash screen and icon

---
+++ app/src/main/java/com/example/tasbeeh/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.tasbeeh;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_DURATION = 2000; // 4 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Delay for 4 seconds and then transition to the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Create an intent to launch the main activity
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish(); // Close the splash screen activity
+            }
+        }, SPLASH_DISPLAY_DURATION);
+    }
+}
